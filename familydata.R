607f45888419afb1cbcd80c7544250b27de4bb04
Zhengguo Gu <z.gu@uvt.nl>
2017-09-21 18:47:59 +0200

    update

--- familydata.R
+++ familydata.R
@@ -1,3 +1,24 @@
+# a function: replace with mean average
+sim_impu <- function(DATA, M_values){
+  nrow <- dim(DATA)[1]
+  ncol <- dim(DATA)[2]
+  DATA <- data.matrix(DATA)
+  
+  for(i in 1:length(M_values)){
+    DATA[which(DATA == M_values[i])] <- NA
+  }
+  
+  for(n in 1:nrow){
+    for(m in 1:ncol){
+      if(is.na(DATA[n, m])){
+          DATA[n, m] <- mean(DATA[n, ], na.rm = T)
+      }
+    }
+  }
+
+ return(DATA)
+}
+
 #This is the script for obtaining the data 
 DS3 = read.csv("D:/TilburgOffice/Dropbox/tilburg office/Research SCA/Project 2 software Simultaneous/newdata/04549-0003-Data.csv", sep = ";" )
 DS4 = read.csv("D:/TilburgOffice/Dropbox/tilburg office/Research SCA/Project 2 software Simultaneous/newdata/04549-0004-Data.csv", sep = ";" )
@@ -5,24 +26,22 @@ DS4 = read.csv("D:/TilburgOffice/Dropbox/tilburg office/Research SCA/Project 2 s
 # some of the questions need to change direction(reverse coding)
 
 # regarding "Please tell us how much you agree with each of the following statements about your relationship with your spouse/partner
-DS3 <- DS3[-which(DS3[, c(352:367)] == 9 | is.na(DS3[, c(352:367)]), arr.ind = T)[,1], ]  #the rows containing missing values are removed
+DS3[, c(352:367)] <- sim_impu(DS3[, c(352:367)], 9)
 DS3[, c(353, 356, 359, 360, 363, 365)] <- 6 - DS3[, c(353, 356, 359, 360, 363, 365)] #reverse coding
 # regarding "These are various ways that couples deal with serious disagreements...
-DS3 <- DS3[-which(DS3[, c(368:371)] == 9 | is.na(DS3[, c(368:371)]), arr.ind = T)[,1], ]
+DS3[, c(368:371)] <- sim_impu(DS3[, c(368:371)], 9)
 DS3[, c(370, 371)] <- 6 - DS3[, c(370, 371)] #so the higher the value the calmer.
 # regarding "when you and one of your children have had a disagreement..."
-DS3 <- DS3[-which(DS3[, c(550:553)] == 9 | is.na(DS3[, c(550:553)]), arr.ind = T)[,1], ]
+DS3[, c(550:553)] <- sim_impu(DS3[, c(550:553)], 9)
 DS3[, 550] <- 5 - DS3[, 550] #note, category 0 to 4
 # regarding "Now we're going to list some statements that deal with ways that you may perceive yourself...
-DS3 <- DS3[-which(DS3[, c(555:568)] == 9 | is.na(DS3[, c(555:568)]), arr.ind = T)[,1], ]
+DS3[, c(555:568)] <- sim_impu(DS3[, c(555:568)], 9)
 DS3[, c(556:562, 565, 566)] <- 5 -  DS3[, c(556:562, 565, 566)]
 
 # for the remaining questionnaires, we moved the missing values
-DS3 <- DS3[-which(DS3[, c(379:404)] == 9 | is.na(DS3[, c(379:404)]), arr.ind = T)[,1], ]
-DS3 <- DS3[-which(DS3[, c(441:460)] == 9 | is.na(DS3[, c(441:460) ]), arr.ind = T)[,1], ]
-DS3 <- DS3[-which(DS3[, c(441:460)] == -8, arr.ind = T)[,1], ]
-DS3 <- DS3[-which(DS3[, c(540:549)] == 9 | is.na(DS3[, c(540:549)]), arr.ind = T)[,1], ]
-
+DS3[, c(379:404)] <- sim_impu(DS3[, c(379:404)], 9)
+DS3[, c(441:460)] <- sim_impu(DS3[, c(441:460)], c(9, -8))
+DS3[, c(540:549)] <- sim_impu(DS3[, c(540:549)], 9)
 
 DS3_NEW <- rowMeans(DS3[, c(352:367)])
 DS3_NEW <- cbind(DS3_NEW, rowMeans(DS3[, c(368:371)]))
@@ -43,23 +62,19 @@ colnames(DS3_NEW)[4:12] <- c("Relationship with partners", "Argue with partners"
 
 ####################################
 # now DS4
-DS4 <- DS4[-which(DS4[, c(176:183)] == 9 | is.na(DS4[, c(176:183)]), arr.ind = T)[,1], ]
+DS4[, c(176:183)] <- sim_impu(DS4[, c(176:183)], 9)
 DS4[, 176] <- 3 - DS4[, 176]  #recoding, answer category 0~3
 
-DS4 <- DS4[-which(DS4[, 304] == 99 | is.na(DS4[, 304])), ] #academic performance
-DS4 <- DS4[-which(DS4[, 304] == 9 | is.na(DS4[, 304])), ]
+DS4[which(DS4[, 304] == 99)] <- NA #academic performance
+DS4[which(DS4[, 304] == 9)] <- NA
 
-DS4 <- DS4[-which(DS4[, c(326:342)] == 9 | is.na(DS4[, c(326:342)]), arr.ind = T)[,1], ]  #extracurricular
-
-DS4 <- DS4[-which(DS4[, c(359:367)] == 9 | is.na(DS4[, c(359:367)]), arr.ind = T)[,1], ]  #Importance of friendship
-
-DS4 <- DS4[-which(DS4[, c(381:397)] == 9 | is.na(DS4[, c(381:397)]), arr.ind = T)[,1], ]  #Self image
+DS4[, c(326:342)] <- sim_impu(DS4[, c(326:342)], 9)
+DS4[, c(359:367)] <- sim_impu(DS4[, c(359:367)], 9)
+DS4[, c(381:397)] <- sim_impu(DS4[, c(381:397)], 9)
 DS4[, c(383,384, 386:388, 390:394, 396,397)] <- 4 - DS4[, c(383,384, 386:388, 390:394, 396,397)] #reverse coding
-
-DS4 <- DS4[-which(DS4[, c(399:418)] == 9 | is.na(DS4[, c(399:418)]), arr.ind = T)[,1], ] #happiness
+DS4[, c(399:418)] <- sim_impu(DS4[, c(399:418)], 9)
 DS4[, c(399, 400, 402, 403, 405:411, 413, 414, 416:418)] <- 3 - DS4[, c(399, 400, 402, 403, 405:411, 413, 414, 416:418)] 
-
-DS4 <- DS4[-which(DS4[, c(468:480)] == 9 | is.na(DS4[, c(468:480)]), arr.ind = T)[,1], ] #confidence about future
+DS4[, c(468:480)] <- sim_impu(DS4[, c(468:480)], 9)
 
 DS4_NEW <- rowMeans(DS4[, 176:183]) #loneliness
 DS4_NEW <- cbind(DS4_NEW, DS4[, 304]) #academic performance
@@ -73,3 +88,9 @@ DS4_NEW <- cbind(DS4[, 1:3], DS4_NEW)
 colnames(DS4_NEW)[4:10] <- c("Loneliness", "Academic performance", "Extracurricular", "Importance of friendship",
                              "Self Image", "Happiness", "Confidence about the future")
 summary(DS4_NEW)
+
+
+# because "family hassels" in DS3_NEW contains too much NA values, better remove this column
+DS3_NEW <- DS3_NEW[, -8]
+# remove NAs
+DS3_NEW <- DS3_NEW[-which(is.na(data.matrix(DS3_NEW)))]
